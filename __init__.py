6d98b4038c381cfd3191008dfb5dfb0f47686495
zuda77 <153694473+zuda77@users.noreply.github.com>
2025-01-11 21:56:10 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,26 @@
+﻿import bpy
+
+bl_info = {
+    "name": "Curve Fitting",
+    "author": "zuda77",
+    "version": (0, 1, 0),
+    "blender": (3, 6, 0),
+    "location": "Edit Mode > Vertex > Curve Fitting",
+    "description": "Perform PCA projection and fit selected vertices to a curve.",
+    "warning": "",
+    "tracker_url": "https://github.com/zuda77/CurveFitting_blender_addon/issues",
+    "wiki_url": "https://github.com/zuda77/CurveFitting_blender_addon" ,
+}
+
+from . import curve_fitting 
+
+def register():
+    curve_fitting.register()
+
+
+def unregister():
+    curve_fitting.register()
+
+
+if __name__ == "__main__":
+    register()
